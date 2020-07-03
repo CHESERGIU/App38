11799ac0d6c57b3f27cb325ac6b29d9ea93e29e3
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:43:42 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace App38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int points = number + 1;
+            for (int i = 0; i <= number; i++)
+            {
+                points += number - i;
+            }
+            Console.WriteLine(points);
+        }
+    }
+}
